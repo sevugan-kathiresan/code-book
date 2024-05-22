0333c98ce4838dc4127687b06dd0d05c928e530a
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-05-22 19:28:56 +1000

    Basic Transpiling Acheived

--- cbook/src/index.tsx
+++ cbook/src/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-  return <h1>Hi</h1>
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState(''); // Result from our esbuild
+
+  //Initializing the esbuild
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    
+    //perform the transpiling
+    const result = await ref.current.transform(input, {
+      loader: 'jsx', // type of code we are providing
+      target: 'es2015' // expected output format
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(
