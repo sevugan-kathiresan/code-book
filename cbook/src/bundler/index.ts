4d9fa497f160769a74a4ca12b25d547d6dcbdda4
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-05-26 18:02:11 +1000

    bundling logic refactored

---
+++ cbook/src/bundler/index.ts
@@ -0,0 +1,31 @@
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
+
+let service: esbuild.Service;
+
+const bundle =  async (rawCode: string) => {
+    // If the esbuild service is not started before means we have to start it
+    if (!service) {
+        service = await esbuild.startService({
+            worker: true,
+            wasmURL: 'https://www.unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+        });
+    }
+
+    const result = await service.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+        define: {
+          'process.env.NODE_ENV': '"production"', // replacing with string production that is why use of two quotations
+          global: 'window', // we need to replace global with window if we try to run our code inside browser
+        }
+      });
+
+      return result.outputFiles[0].text;
+
+};
+
+export default bundle;
--- cbook/src/index.tsx
+++ cbook/src/index.tsx
@@ -1,47 +1,18 @@
 import React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  //Initializing the esbuild
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://www.unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-    
-    //building = transpiling + bundling
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"', // replacing with string production that is why use of two quotations
-        global: 'window', // we need to replace global with window if we try to run our code inside browser
-      }
-    });
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
   
   return (
